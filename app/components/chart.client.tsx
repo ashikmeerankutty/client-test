63da660ae8cc5ce7ef44d52b38ee4260e9106633
Ashik Meerankutty <ashik9591@gmail.com>
2021-12-13 21:14:23 +0530

    Initializing test repo

---
+++ app/components/chart.client.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ClientTest: React.FC = () => {
+    return <div>Chart</div>
+}
---
+++ app/routes/chart.tsx
@@ -0,0 +1,9 @@
+import { ClientTest } from "~/components/chart.client";
+
+export default function Index() {  
+    return (
+      <div className="remix__page">
+        <ClientTest />
+      </div>
+    );
+  }
