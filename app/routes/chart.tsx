e95c0b89c2b4e889557ae4affcd9ca4ba5a50a0b
Kent C. Dodds <me+github@kentcdodds.com>
2021-12-14 08:55:24 -0700

    render client-only on the client only

--- app/routes/chart.tsx
+++ app/routes/chart.tsx
@@ -1,9 +1,10 @@
+import * as React from "react";
 import { ClientTest } from "~/components/chart.client";
 
-export default function Index() {  
-    return (
-      <div className="remix__page">
-        <ClientTest />
-      </div>
-    );
-  }
+export default function Index() {
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  return <div className="remix__page">{mounted ? <ClientTest /> : null}</div>;
+}
